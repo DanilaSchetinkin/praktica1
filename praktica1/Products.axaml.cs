7aaf76631a04cbe628731f2d516f464d956d3697
DanilaSchetinkin <danya.sch2015@gmail.com>
2025-09-08 18:38:27 +0300

    product

--- praktica1/Products.axaml.cs
+++ praktica1/Products.axaml.cs
@@ -1,13 +1,47 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+using praktica1.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace praktica1;
 
 public partial class Products : Window
 {
+
+    public class Tovars
+    {
+        //public Bitmap? ImagePath { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductCost { get; set; }
+        public string ProductCaption { get; set; }
+    }
+
+    private ObservableCollection<Tovars> _tovars;
+
     public Products()
     {
         InitializeComponent();
+        LoadData();
+    }
+
+
+    private void LoadData()
+    {
+        using var context = new ShchetinkinContext();
+        _tovars = new ObservableCollection<Tovars>(context.Products
+            .Select(s => new Tovars
+            {
+                //ImagePath = new Bitmap(AppDomain.CurrentDomain.BaseDirectory + "/" + s.ProductImage),
+                ProductName = s.ProductName,
+                ProductCost = s.ProductCost,
+                ProductCaption = s.ProductCaption
+            })
+            .ToList());
+
+        TovarBox.ItemsSource = _tovars;
     }
 }
