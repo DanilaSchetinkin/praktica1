5a37ed0c8de361cf5e028d9887d276e6253dc11a
DanilaSchetinkin <danya.sch2015@gmail.com>
2025-09-08 13:56:06 +0300

    init

--- praktica1/obj/Debug/net8.0/praktica1.AssemblyInfo.cs
+++ praktica1/obj/Debug/net8.0/praktica1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("praktica1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6463247e459d648ebd7ae203a23f6d02610613dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("praktica1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("praktica1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
